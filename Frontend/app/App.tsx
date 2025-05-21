21325f938717850127490975e1d64d3d2c816201
Jazmín Quinteros <jazmin.quinteros21@gmail.com>
2025-05-20 23:37:13 -0300

    delete redundancy in App.ts

--- Frontend/app/App.tsx
+++ Frontend/app/App.tsx
@@ -1,23 +1,8 @@
 import * as React from "react";
 import { ContextStateProvider } from "../contexts/ContextState";
 import Router from "./Router";
-import { useFonts } from "expo-font";
-import { FONTS } from "@/src/constants/fonts";
-import { SplashScreen } from "expo-router";
-import { useEffect } from "react";
-import { useCustomFonts } from "@/src/hooks/useFonts";
-
-SplashScreen.preventAutoHideAsync();
 
 const App = () => {
-  const [fontsLoaded] = useCustomFonts();
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) return null;
   
   return (
     <ContextStateProvider>
